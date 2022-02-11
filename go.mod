a509691583b21928fab6d580c7f979a7722ee5ba
Vishnu Chanderraju <vishnu.c@AFFINITYMAC-090.local>
2022-02-11 16:00:12 +0800

    prepare script + tests

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/jaihind213/auto_proto_registry_loader
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/iancoleman/strcase v0.2.0 // indirect
+	google.golang.org/protobuf v1.27.1
+)
---
+++ lookup_test.go
@@ -0,0 +1,15 @@
+package pocket_protobuff_registry
+
+import (
+	_ "github.com/jaihind213/auto_proto_registry_loader/go_bindings/pocket_registry"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/reflect/protoregistry"
+	"testing"
+)
+
+func TestLookup(t *testing.T) {
+	_, err := protoregistry.GlobalTypes.FindMessageByName(protoreflect.FullName("foo.bar.Keeper"))
+	if err != nil {
+		t.Error("error in looking up type Foo. it should exist in registry but does not ! ," + err.Error())
+	}
+}
